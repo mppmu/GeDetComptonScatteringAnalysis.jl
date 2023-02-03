b31e047466c5383767e0692a077ff527955f28ad
Felix Hagemann <hagemann@mpp.mpg.de>
2023-02-03 14:48:31 +0100

    Add tests for the ICPC setup with a single czt
    In the current version of the package, the CZT alignment quantities are
    constant values. However, they are different for the segBEGe and the
    ICPC detector. This should be changed to be something passed from the
    outside to the functions in the future.

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,7 +13,7 @@ destdir = joinpath(@__DIR__, "results")
     z = 62.8         # height of czt cameras (in mm)
     name = "segBEGe" # group name of detector in raw lh5 files
 
-    @testset "only core" begin
+    @testset "only core, czt and czt2" begin
         r = 81.8     # radial position of radiation source (in mm)
         stack_and_merge_at_z(datapath, destdir, r, phi, z, hv, name)
         resultfile = joinpath(destdir, "R_81.8mm_Z_62.8mm_Phi_88.5deg_T_71.75K_measuretime_1200sec_HV_300V-20230126T145729Z-preprocessed.lh5")
@@ -36,5 +36,19 @@ destdir = joinpath(@__DIR__, "results")
 end
 
 @testset "ICPC" begin
-    # Add a test-set from ICPC here
+    hv = 1200.       # applied Voltage
+    phi = 69.9       # azimuthal position of radiation source (in degrees)
+    z = 25.0         # height of czt cameras (in mm)
+    name = "ICPC"    # group name of detector in raw lh5 files
+
+    # G.x_CZT, G.y_CZT, G.z_CZT = 1000u"μm" .* (-6.404541969299316, 70.71229063689407 + 2.5, 72.9764303564702 - 87)
+
+    @testset "only core and czt" begin
+        r = 71.0     # radial position of radiation source (in mm)
+        stack_and_merge_at_z(datapath, destdir, r, phi, z, hv, name, idx_c=16)
+        resultfile = joinpath(destdir, "R_71.0mm_Z_25.0mm_Phi_69.9deg_T_95.0K_measuretime_600sec_HV_1200.0V-20220919T141651Z-preprocessed.lh5")
+        @test isfile(resultfile)
+        mtime, R, Z = get_all_z(destdir, name = name, center = 82.12404619872268)
+        rm(resultfile)
+    end
 end
