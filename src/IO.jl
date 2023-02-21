9b31a7c0ca645cd222ead42b745048b66d5526cc
Felix Hagemann <hagemann@mpp.mpg.de>
2023-02-21 20:20:41 +0100

    Specify types in `IO.jl`

--- src/IO.jl
+++ src/IO.jl
@@ -1,28 +1,33 @@
 # This file is a part of GeDetComptonScatteringAnalysis.jl, licensed under the MIT License (MIT).
 
-regR = "R_(?<R>\\d+?\\.\\d+)mm"
-regPhi = "_Phi_(?<Phi>\\d+?\\.\\d+)deg"
-regT = "_T_(?<T>\\d+?\\.\\d+)K"
-regZ = "_Z_(?<Z>\\d+?\\.\\d+)mm"
-regM = "_measuretime_(?<time>\\d+?)sec"
-regV = "HV_(?<voltage>[\\d\\.]+?)V"
-getR(s::String) = parse(Float64, match(Regex(regR), s).captures[1])
-getPhi(s::String) = parse(Float64, match(Regex(regPhi), s).captures[1])
-getT(s::String) = parse(Float64, match(Regex(regT), s).captures[1])
-getZ(s::String) = parse(Float64, match(Regex(regZ), s).captures[1])
-getM(s::String) = parse(Float64, match(Regex(regM), s).captures[1])
-getV(s::String) = parse(Float64, match(Regex(regV), s).captures[1])
+const regR = "R_(?<R>\\d+?\\.\\d+)mm"
+const regPhi = "_Phi_(?<Phi>\\d+?\\.\\d+)deg"
+const regT = "_T_(?<T>\\d+?\\.\\d+)K"
+const regZ = "_Z_(?<Z>\\d+?\\.\\d+)mm"
+const regM = "_measuretime_(?<time>\\d+?)sec"
+const regV = "HV_(?<voltage>[\\d\\.]+?)V"
+
+@inline getR(s::AbstractString)   = parse(Float64, match(Regex(regR), s).captures[1])
+@inline getPhi(s::AbstractString) = parse(Float64, match(Regex(regPhi), s).captures[1])
+@inline getT(s::AbstractString)   = parse(Float64, match(Regex(regT), s).captures[1])
+@inline getZ(s::AbstractString)   = parse(Float64, match(Regex(regZ), s).captures[1])
+@inline getM(s::AbstractString)   = parse(Float64, match(Regex(regM), s).captures[1])
+@inline getV(s::AbstractString)   = parse(Float64, match(Regex(regV), s).captures[1])
 
 ############################
 # filtered -> preprocessed #
 ############################
-function fetch_relevant_filtered_files(sourcedir, phi, z, r, hv_in_fname, n_max_files)
-    allfiles = filter(file -> endswith(file, "filtered.h5")
+function fetch_relevant_filtered_files(
+        sourcedir::AbstractString, phi::Number, z::Number, r::Number, 
+        hv_in_fname::Bool, n_max_files::Int
+    )::Vector{String}
+
+    allfiles::Vector{String} = filter(file -> endswith(file, "filtered.h5")
                             && occursin("Phi_$(phi)", file)
                             && occursin("Z_$(z)", file)
                             && occursin("R_$(r)", file), readdir(sourcedir))
     if hv_in_fname
-        allfiles = filter(file -> contains(file, "_HV_$hv"), allfiles)
+        allfiles = filter(file -> occursin("_HV_$(hv)", file), allfiles)
     end
     if n_max_files > 0
         allfiles = allfiles[1:min(length(allfiles), n_max_files)]
@@ -41,7 +46,7 @@ function read_filtered_file(f::AbstractString, name::AbstractString)
     end
 end
 
-function build_output_prepocessed_file_name(files, destdir, n)
+function build_output_prepocessed_file_name(files::Vector{String}, destdir::AbstractString, n::Int)::String
     destdir = joinpath(destdir, "")
     fpos, ftime = split(basename(files[end]), "measuretime_")
     fmtime, fdate = split(ftime, "sec")
@@ -51,9 +56,10 @@ function build_output_prepocessed_file_name(files, destdir, n)
         fpos * "measuretime_$(fmtime)sec" * ending * "preprocessed.lh5")
 end
 
-write_preprocessed_file(f::LHDataStore, name::String , data::Tuple{Table, Table}) = begin
+function write_preprocessed_file(f::LHDataStore, name::AbstractString , data::Tuple{Table, Table})::Nothing
     LegendHDF5IO.writedata(f.data_store, "$name", data[1])
     LegendHDF5IO.writedata(f.data_store, "czt", data[2])
+    nothing
 end
 
 
@@ -64,7 +70,7 @@ function is_preprocessed_file(f::AbstractString)::Bool
     return (endswith(f, "-preprocessed.lh5") || throw(ArgumentError, "Expected prepocessed file as input.")) && isfile(f)
 end
 
-function read_preprocessed_file(f::AbstractString, name::AbstractString)
+function read_preprocessed_file(f::AbstractString, name::AbstractString)::Tuple{Table, Table}
     @assert is_preprocessed_file(f)
     LHDataStore(f) do lhd
         lhd[name][:], lhd["czt"][:]
