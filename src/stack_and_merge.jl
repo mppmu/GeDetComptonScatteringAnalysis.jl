d3cc024cbabb57f0b893efd2bc3142401d60c316
Felix Hagemann <hagemann@mpp.mpg.de>
2023-02-27 20:33:47 +0100

    Move `try`-block for problematic files to `stack_and_merge_at_z`

--- src/stack_and_merge.jl
+++ src/stack_and_merge.jl
@@ -16,34 +16,33 @@ _vcat!(x::Tuple{Tuple{detTable, cztTable}, Int}, y::Tuple{detTable, cztTable}) =
 function read_and_merge_filtered_file(
         file::AbstractString, name::AbstractString; #hv::Number; 
         idx_c::Int = 1, #corr_daq_energy::Bool = false, rm_pileup::Bool = false
-    )
-    try
-        det::detTable, czt1::cztTable, czt2 = read_filtered_file(file, name)
-        core::detTable = det[findall(det.chid .== idx_c)]
-        motor_z::QuantityMM{Float64} = getZ(file)
-        czt::cztTable = merge_cameras_and_transform_coordinates(core, czt1, czt2, motor_z)
-        # TODO: reintroduce rm_pileup with idx that also works for detectors with multiple channels
-        # rm_pileup && (idx = intersect(findall(is_not_peak_pileup, core.samples), idx))
-        det, czt
-    catch e
-        @warn "$file was ignored due to possible file issues"
-        (missing, missing)
-    end
+    )::Tuple{detTable, cztTable}
+
+    det::detTable, czt1::cztTable, czt2 = read_filtered_file(file, name)
+    core::detTable = det[findall(det.chid .== idx_c)]
+    motor_z::QuantityMM{Float64} = getZ(file)
+    czt::cztTable = merge_cameras_and_transform_coordinates(core, czt1, czt2, motor_z)
+    # TODO: reintroduce rm_pileup with idx that also works for detectors with multiple channels
+    # rm_pileup && (idx = intersect(findall(is_not_peak_pileup, core.samples), idx))
+    det, czt
 end
 
 function stack_and_merge_at_z(
         sourcedir::AbstractString, destdir::AbstractString, 
         r::Number, phi::Number, z::Number, hv::Number, name::AbstractString; 
-        idx_c::Int = 1, corr_daq_energy::Bool = false, rm_pileup::Bool = false,
+        idx_c::Int = 1, #corr_daq_energy::Bool = false, rm_pileup::Bool = false,
         hv_in_filename::Bool = false, n_max_files::Int = -1, verbose::Bool = true
     )::Nothing 
 
     files = fetch_relevant_filtered_files(sourcedir, phi, z, r, hv_in_filename, n_max_files)
     x = ((missing, missing), 0)
     for file in files
-        x = _vcat!(x, 
-        read_and_merge_filtered_file(file, 
-            name, hv; idx_c, corr_daq_energy, rm_pileup))
+        try
+            x = _vcat!(x, 
+            read_and_merge_filtered_file(file, name; idx_c))
+        catch e
+            @warn "$file was ignored due to possible file issues"
+        end
     end
     verbose && println("$(x[2]) / $(length(files)) successful")
     fileout = build_preprocessed_file_name(files, destdir, x[2])
