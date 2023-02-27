e5396244d8b12f7d0249ac7a83a9b1da620d98f7
Felix Hagemann <hagemann@mpp.mpg.de>
2023-02-27 22:38:05 +0100

    Remove `econv` dictionary and update `get_econv`
    The method `get_econv` now takes a `detTable` as an argument,
    allowing for a precise determination of `econv` for each radius
    individually rather than averaged over all radii

--- src/energies.jl
+++ src/energies.jl
@@ -1,15 +1,15 @@
 const Cs_energy = 661.66u"keV"
 
-const econv = Dict{Float64, Float64}( 
-    300.0 => 0.000746364303766692,
-    # 600.0 => 0.0007730410437211994,
-    600.0 => 0.0006952481325306902,
-    # 900.0 => 0.0006666592487136949,
-    900.0 => 0.0006781005477809491,
-    1200.0 => 0.0006189596091123743, 
-    3500.0 => 0.0005490928284752016,
-    3000.0 => 0.000653
-)
+# const econv = Dict{Float64, Float64}( 
+#     300.0 => 0.000746364303766692,
+#     # 600.0 => 0.0007730410437211994,
+#     600.0 => 0.0006952481325306902,
+#     # 900.0 => 0.0006666592487136949,
+#     900.0 => 0.0006781005477809491,
+#     1200.0 => 0.0006189596091123743, 
+#     3500.0 => 0.0005490928284752016,
+#     3000.0 => 0.000653
+# )
 
 
 # ----- INFO ------
@@ -44,36 +44,46 @@ DAQ_energy from the core. `idx_c` denotes the index of the core segment,
 `nseg` the number of segments ,`max` the maximal raw value to 
 consider in the histogram and `name` the name of the table
 """
-function get_econv(sourcedir::AbstractString; 
+# function get_econv(sourcedir::AbstractString;
+#         idx_c::Int = 1, bsize::Int = 1000, max::Int = 1_500_000, 
+#         name::AbstractString = "segBEGe", verbose::Bool = true
+#     )::typeof(Cs_energy)
+
+#     E::Vector{Int32} = Int32[]
+#     files::Vector{String} = readdir(sourcedir)
+    
+#     if verbose 
+#         N::Int = length(files)
+#         prog = Progress(N, "Reading $(N) file" * (N != 1 ? "s" : ""), 1) 
+#     end
+
+#     for file in files
+#         try
+#             lhd = LHDataStore(joinpath(sourcedir, file))
+#             core_e = get_daqe(lhd, name, idx_c)
+#             append!(E, core_e)
+#         catch
+#             nothing
+#         end
+#         verbose && next!(prog)
+#     end
+#     h = fit(Histogram{Float64}, E, (1:bsize:max))
+#     _, peakpos = RadiationSpectra.peakfinder(h)
+#     Cs_energy / peakpos[1]
+# end
+
+# function get_daqe(lhd::LHDataStore, name::AbstractString, idx_c::Int)
+#     daqe = lhd[name * "/DAQ_energy"][:]
+#     chid = lhd[name * "/chid"][:]
+#     daqe[findall(x -> x == idx_c, chid)]
+# end
+
+function get_econv(det::detTable;
         idx_c::Int = 1, bsize::Int = 1000, max::Int = 1_500_000, 
-        name::AbstractString = "segBEGe", verbose::Bool = true
+        verbose::Bool = true
     )::typeof(Cs_energy)
 
-    E::Vector{Int32} = Int32[]
-    files::Vector{String} = readdir(sourcedir)
-    
-    if verbose 
-        N::Int = length(files)
-        prog = Progress(N, "Reading $(N) file" * (N != 1 ? "s" : ""), 1) 
-    end
-
-    for file in files
-        try
-            lhd = LHDataStore(joinpath(sourcedir, file))
-            core_e = get_daqe(lhd, name, idx_c)
-            append!(E, core_e)
-        catch
-            nothing
-        end
-        verbose && next!(prog)
-    end
-    h = fit(Histogram{Float64}, E, (1:bsize:max))
+    h::Histogram{Float64} = fit(Histogram{Float64}, det.DAQ_energy[findall(det.chid .== idx_c)], (1:bsize:max))
     _, peakpos = RadiationSpectra.peakfinder(h)
     Cs_energy / peakpos[1]
-end
-
-function get_daqe(lhd::LHDataStore, name::AbstractString, idx_c::Int)
-    daqe = lhd[name * "/DAQ_energy"][:]
-    chid = lhd[name * "/chid"][:]
-    daqe[findall(x -> x == idx_c, chid)]
 end
